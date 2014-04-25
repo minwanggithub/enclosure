011f4b5a6780bb8e5f18e8b36452de6a486e1875
Jean Liu <DEV\I54347>
2014-04-25 11:45:54 -0700

    3eCompli - TRECOMPLI-353 - Create user profile for set up the default value when create a new document

---
+++ roamingprofile-main.js
@@ -0,0 +1,37 @@
+﻿;
+(function($) {
+    if ($.fn.complibRoamingProfile == null) {
+        $.fn.complibRoamingProfile = {};
+    }
+
+    $.fn.complibRoamingProfile = function() {
+
+        //local variables
+        var docConfigObj = $('#DocumentConfiguration');
+
+        //Public functions
+        docConfigObj.on("click", "#btnDocumentConfigSave", function (e) {
+            e.preventDefault();
+         
+            var form = $("#documentConfigForm");
+            var url = form.attr("action");
+            var formData = form.serialize();
+          
+            $.post(url, formData, function (data) {
+                if (data == "Document configuration Saved") {
+                    $('#CreatedMessage').fadeIn(500).delay(1000).fadeOut(400).html(data);
+                    return true;
+                } else {
+                    alert('Error occured while saving document configuration, ' + data);
+                    return false;
+                }
+            });
+        });
+
+        return {
+            
+        };
+
+    };
+
+})(jQuery);
