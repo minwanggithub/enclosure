96cbf42fb4a3679d38c1411637c32e9d5ec5b469
Min Wang <DEV\i67396>
2019-01-14 14:01:18 -0800

    eeeCompli:Jira#3334:QC - Xref - SDS requests - pdf icon produces 404 server error

--- eeecompli.bundlesibling.js
+++ eeecompli.bundlesibling.js
@@ -54,8 +54,7 @@
                 var bs = checkBox.get(0).kendoBindingTarget.source;
                 //var bs = $(siblingRows[index]).get(0).children[0].kendoBindingTarget.source;
                 //kendoConsole.log("Include: " + bs.included + " , DocumentId: " + bs.documentId + ", RevisionTitle: " + bs.exteredRevisionTitle);
-
-                if (bs != null && bs.included && bs.exteredRevisionTitle === "") {
+                if (bs != null && bs.included && bs.enteredRevisionTitle === "") {
                     alert("Please enter revision title for document: " + bs.documentId);
                     e.preventDefault();                    
                     return;
