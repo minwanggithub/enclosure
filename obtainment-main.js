7a1ad3bb19d0b96b86fb8ec45b10d1d7ea7b5c89
Arindam Kataki <DEV\i57259>
2019-03-22 15:40:31 -0700

    Typo.

--- obtainment-main.js
+++ obtainment-main.js
@@ -182,7 +182,7 @@
                 NextStepMissing: "Obtainment next step has not been selected.",
                 OneOrMoreSelectionsNotRevisions: "One or more of the selected item(s) are not valid. The 'Save as Current' action can only be perfromed on Revisions.",
                 InvalidSubstitutionTokens: "Invalid or incorrect substitution tokens. ",
-                NotificationRecepientMissing: "Super email notification recepient missing.",
+                NotificationRecepientMissing: "Super email notification recipient missing.",
                 NoObtainmentWorkItemSelected: "No obtainment work item has been selected selected",
                 EmailBodyHasEmbeddedUrls: "Email body has embeddded URL(s)"
             }
